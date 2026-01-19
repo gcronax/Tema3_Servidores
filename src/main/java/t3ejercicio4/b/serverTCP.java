854233a29e932d8c17a292c7fdeb357d518ae039
guicamher <guillecampher15@gmail.com>
2026-01-19 19:08:18 +0100

    a

--- src/main/java/t3ejercicio4/b/serverTCP.java
+++ src/main/java/t3ejercicio4/b/serverTCP.java
@@ -1,37 +1,43 @@
 package t3ejercicio4.b;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class serverTCP {
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(12345);
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
+ public static void main(String[] args) {
+    try {
+            ServerSocket serverSocket = new
+                    ServerSocket(12345);
+             while (true) {
+                 Socket clientSocket = serverSocket.accept();
 
-                try {
-                    OutputStream outputStream = clientSocket.getOutputStream();
-                    PrintWriter out = new PrintWriter(outputStream,
-                            true);
-                    out.println("Hola, cliente! eres: "+clientSocket.getRemoteSocketAddress());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                 try {
+                     OutputStream outputStream =
+                            clientSocket.getOutputStream();
+                     PrintWriter out = new
+                            PrintWriter(outputStream,
+                             true);
+                     out.println("Hola, cliente! eres: "+clientSocket.getRemoteSocketAddress());
+                     } catch (IOException e) {
+                     e.printStackTrace();
+                    }
 
 
                 InputStream inputStream =
                         clientSocket.getInputStream();
                 BufferedReader in = new BufferedReader(new
-                        InputStreamReader(inputStream));
-                String respuesta = in.readLine();
+                InputStreamReader(inputStream));
+
+
+                float respuesta = Float.parseFloat(in.readLine());
                 System.out.println("Mensaje del cliente: " +
-                        respuesta);
+                        (respuesta));
                 clientSocket.close();
-            }
-        } catch (IOException e) {
+                }
+            } catch (IOException e) {
             e.printStackTrace();
+            }
         }
-    }
 }
